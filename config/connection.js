b924e3b03c9f2be5ea5e0ae2266af10ddc86bfcb
JD <jdfili14@gmail.com>
2019-02-20 22:45:33 -0800

    gitignre

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: db.password,
+    password: 'Dw2cb25141414!@',
     database: "burgers_db"
   });
 };
